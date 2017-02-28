ed5ed5a349022b33a67bbf96adf935fc1645d12e
eimear <eimear.flynn1@gmail.com>
2017-02-28 15:16:57 +0000

    Improved quiz menu

--- app/src/main/java/com/example/dacapo/QuizMenuActivity.java
+++ app/src/main/java/com/example/dacapo/QuizMenuActivity.java
@@ -5,33 +5,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class QuizMenuActivity extends AppCompatActivity {
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_menu);
-    }
 
-    public void goToQuiz1(View view){
-        Bundle b = new Bundle();
-        b.putInt("level", 1);
-        Intent intent = new Intent(this, QuizActivity.class);
-        intent.putExtras(b);
-        startActivity(intent);
-    }
-    public void goToQuiz2(View view){
-        Bundle b = new Bundle();
-        b.putInt("level", 2);
-        Intent intent = new Intent(this, QuizActivity.class);
-        intent.putExtras(b);
-        startActivity(intent);
-    }
-    public void goToQuiz3(View view){
-        Bundle b = new Bundle();
-        b.putInt("level", 3);
-        Intent intent = new Intent(this, QuizActivity.class);
-        intent.putExtras(b);
-        startActivity(intent);
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListData = ExpandableListData.getData();
+        expandableListTitle = new ArrayList<String>(expandableListData.keySet());
+        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitle, expandableListData);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Bundle b = new Bundle();
+                b.putInt("grade", groupPosition);
+                b.putInt("level", childPosition);
+                Intent intent = new Intent(QuizMenuActivity.this, QuizActivity.class);
+                intent.putExtras(b);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 }
